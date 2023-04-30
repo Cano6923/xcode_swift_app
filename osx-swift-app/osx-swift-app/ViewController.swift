05620c1d44de93d8c9634a40da5be70c5002fea5
Cano6923 <131813741+Cano6923@users.noreply.github.com>
2023-04-30 00:45:43 -0600

    macos mojave 10.14.6
    xcode 10,3 10G8

---
+++ osx-swift-app/osx-swift-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  osx-swift-app
+//
+//  Created by Admin on 30/04/2023.
+//  Copyright © 2023 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
